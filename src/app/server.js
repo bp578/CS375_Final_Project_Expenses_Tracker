ef70b468476ce14509142227c4d9c5a26f9e9303
bp578 <138180280+bp578@users.noreply.github.com>
2023-08-12 22:36:13 -0400

    "add" event handler added
    "add" event handler does nothing until we figure out how we store expenses in the database.

--- src/app/server.js
+++ src/app/server.js
@@ -1,124 +1,71 @@
 let express = require("express");
 let { Pool } = require("pg");
-let argon2 = require("argon2");
-let cookieParser = require("cookie-parser");
-let crypto = require("crypto");
 
 let env = require("../../env.json");
 let hostname = "localhost";
 let port = 3000;
 let app = express();
-let pool = new Pool(env);
+let pool = new Pool();
 
-app.use(express.static("src/app/public"));
+app.use(express.static("public"));
 app.use(express.json());
-pool.connect().then(() => {
-    console.log(`Connected to database: ${env.database}`);
-});
 
-let tokenStorage = {};
-
-let cookieOptions = {
-    httpOnly: true,
-    secure: true,
-    sameSite: "strict",
-};
-
-app.post('/create', async (req,res) => {
+app.post('/create', (req, res) => {
+    console.log(req.body);
     let user = req.body["user"];
     let pass = req.body["pass"];
     let re = req.body["re"];
 
-    console.log("New Account Info:", req.body);
+    if ((user === '') || (pass === '') || (re === '')) {
+        return res.status(400).json({ error: "One or more entries is missing!" });
+    }
 
-    if ((user === '') || (pass === '') || (re === '')){
-        return res.status(400).json({error: "One or more entries is missing!"});
-    };
-    
-    if (pass !== re){
-        return res.status(400).json({error: "Passwords do not match. Please re-enter password"});
+    if (pass !== re) {
+        return res.status(400).json({ error: "Passwords do not match. Please re-enter password" });
     };
 
-    if (user.length >= 50 || pass.length >= 50){
-        return res.status(400).json({error: "Username or pass is longer than 50 characters"});
-    };
-
-    let hash;
-    try {
-        hash = await argon2.hash(pass);
-    } catch (error) {
-        console.log("HASH FAILED:", error);
-        return res.status(500).json({error: "Failed to Hash password"});
-    };
-
-    console.log("Hash:", hash);
-    // Check if the username already exist
-    pool.query(
-        `SELECT 1 FROM accounts WHERE username = $1`, [user]
-        ).then(result => {
-            if (result.rowCount >= 1){
-                return res.status(400).json({error: "Username already exist. Please use another username"});
-            } else {
-                // Create new account if is doesnt exists already
-                pool.query(
-                    `INSERT INTO accounts (username, password) VALUES ($1, $2) RETURNING *`, [user, hash]
-                    ).then(result => {
-                        console.log("Inserted:");
-                        console.log(result.rows);
-                        return res.json({success: "Account has been successfully created. Please return to the login menu to login"});
-                    }).catch(error => {
-                        console.log("Could not add new account:");
-                        console.log(error);
-                    });
-            };
-        }).catch(error => {
-            console.log("Verifying existing user failed:");
-            console.log(error);
-        });
+    res.json({ success: "Account has been successfully added. Please return to the login menu" });
 });
 
-app.get(`/login`, async (req, res) => {
+app.get(`/login`, (req, res) => {
     let user = req.query.user;
     let pass = req.query.pass;
+    console.log(req.query);
 
-    console.log("Login attempt:", req.query);
-
-    if (user === '' || pass === ''){
-        return res.status(400).json({error: "One or more entries is missing. Unsuccessful login"})
+    if (user === '' || pass === '') {
+        return res.status(400).json({ error: "One or more entries is missing. Unsuccessful login" })
     };
 
-    let queryRes;
-    try {
-        queryRes = await pool.query(`SELECT password FROM accounts WHERE username = $1`, [user]);
-    } catch (error){
-        console.log("QUERIED FAILED:", error);
-        return res.status(500).json({error: "QUERIED FAILED"});
-    };
+    res.status(304).redirect('/land.html');
+});
 
-    if (queryRes.rowCount === 0){
-        return res.status(500).json({error: "Username does not exist"});
-    };
+//Add transaction
+app.post('/add', (req, res) => {
+    let transaction = req.query.transaction;
+    let category = req.query.category;
+    let amount = req.query.amount;
 
-    let hash = queryRes.rows[0].password;
-    let verifyResult;
-    try {
-        verifyResult = await argon2.verify(hash, pass);
-    } catch (error) {
-        console.log("VERIFYING FAILED", error);
-        return res.status(500).json({error: "VERIFY FAILED"});
-    };
+    if (!addQueryIsValid(req.query)) {
+        return res.status(400).json({ error: "Error with query parameters" });
+    }
 
-    if (!verifyResult) {
-        console.log("Credentials didn't match");
-        return res.status(400).json({error: "Incorrect Password"});
-    } else {
-        let token = crypto.randomBytes(32).toString("hex");
-        tokenStorage[token] = user;
-        console.log(tokenStorage);
-        return res.cookie("token", token, cookieOptions).json({url: `http://${hostname}:${port}/land.html`});
-    };
+    //Add to database once that is implemented. Return an empty json body for now.
+    console.log("Add request recieved");
+    return res.status(200).json({});
+
 });
 
 app.listen(port, hostname, () => {
-    console.log(`Connecting to: http://${hostname}:${port}`);
+    console.log(`Connecting to http://${hostname}:${port}`);
 });
+
+//Validation
+function addQueryIsValid(query) {
+    if (!query.transaction || !query.category || !query.amount) {
+        return false;
+    }
+
+    //Any category is valid for now. Maybe add a list of all valid categories later?
+    return (Number.isFinite(Number.parseInt(query.amount)));
+
+}
